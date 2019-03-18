5f038dc81a3abad786a4116be997131f628774d9
Paula Tulis <tulis.paula@gmail.com>
2019-03-18 00:15:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,11 @@
  def my_collect(collection)
   i=0 
+  new_collection = []
   while i < collection.length 
   yield(collection[i])
   i= i+1 
 end
-collection 
+new_collection 
 end 
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
-
 
 
